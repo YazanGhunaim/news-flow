0578f5cbcb93102dd27ae9afaeed705dd1bab8b7
yazan ghunaim <yazanghunaim07@gmail.com>
2025-04-07 14:45:06 +0200

    Floating animated button

---
+++ newsflow-client/newsflow-client/Core/Components/FloatingButton.swift
@@ -0,0 +1,49 @@
+//
+//  FloatingButton.swift
+//  newsflow-client
+//
+//  Created by Yazan Ghunaim on 4/7/25.
+//
+
+import SwiftUI
+
+struct FloatingButton: View {
+    @State private var size: CGFloat = 75
+    @State private var scale: CGFloat = 1.5
+
+    let image: String
+    let action: () -> Void
+
+    var body: some View {
+        Circle()
+            .fill(Color.gray)
+            .stroke(.primary, lineWidth: 0.5)
+            .frame(width: size, height: size)
+            .shadow(color: Color.NFPrimary, radius: 8)
+            .overlay {
+                Image(systemName: image)
+                    .scaleEffect(scale)
+            }
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    self.size = 100
+                    self.scale = 2
+                }
+                // Animate back after time delay
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        self.size = 75
+                        self.scale = 1.5
+                    }
+                }
+                action()
+            }
+    }
+}
+
+#Preview {
+    FloatingButton(image: "airpods.max") {
+        print("You pressed me!")
+    }
+    .preferredColorScheme(.light)
+}
