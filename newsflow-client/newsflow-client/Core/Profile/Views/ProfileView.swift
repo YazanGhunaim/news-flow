0310fcc98f70d0371f7705ad289cdc8c3aa8e849
yazan ghunaim <yazanghunaim07@gmail.com>
2025-04-11 19:00:16 +0200

    refactoring of profile view

--- newsflow-client/newsflow-client/Core/Profile/Views/ProfileView.swift
+++ newsflow-client/newsflow-client/Core/Profile/Views/ProfileView.swift
@@ -13,54 +13,55 @@ struct ProfileView: View {
 
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 24) {
-                // MARK: User data
-                if let user = viewmodel.user {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Username: @\(user.username)")
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 24) {
+                    // MARK: User data
+                    if let user = viewmodel.user {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Username: @\(user.username)")
 
-                        Text("Email: \(user.email)")
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.gray.opacity(0.1))
-                    .font(.subheadline.bold())
-                    .cornerRadius(8)
-                }
-
-                // MARK: Edit and Settings buttons
-                HStack(spacing: 16) {
-                    CustomButton(enabled: true, height: 40, text: "Settings") {
-                        router.navigate(to: .profileSettingsView)
+                            Text("Email: \(user.email)")
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.gray.opacity(0.1))
+                        .font(.subheadline.bold())
+                        .cornerRadius(8)
                     }
 
-                    CustomButton(enabled: true, height: 40, text: "Edit Profile") {
-                        router.navigate(to: .editProfileView)
+                    // MARK: Edit and Settings buttons
+                    HStack(spacing: 16) {
+                        CustomButton(enabled: true, height: 40, text: "Settings") {
+                            router.navigate(to: .profileSettingsView)
+                        }
+
+                        CustomButton(enabled: true, height: 40, text: "Edit Profile") {
+                            router.navigate(to: .editProfileView)
+                        }
                     }
-                }
 
-                // MARK: Bookmarks
-                if let bookmarks = viewmodel.bookmarks, !bookmarks.isEmpty {
-                    VStack(alignment: .leading, spacing: 16) {
-                        Text("Bookmarked Articles")
-                            .font(.headline)
+                    // MARK: Bookmarks
+                    if let bookmarks = viewmodel.bookmarks, !bookmarks.isEmpty {
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text("Bookmarked Articles")
+                                .font(.headline)
 
-                        ScrollView(showsIndicators: false) {
                             VStack(alignment: .leading, spacing: 16) {
                                 ForEach(bookmarks) { article in
                                     ArticleCell(article: article)
                                         .onTapGesture { router.navigate(to: .articleView(article: article)) }
                                 }
                             }
+
                         }
                     }
-                }
 
-                Spacer()
+                    Spacer()
+                }
+                .padding()
+                .task { await viewmodel.fetchBookmarks() }
+                .navigationTitle("\(viewmodel.user?.name ?? "Profile")")
             }
-            .padding()
-            .task { await viewmodel.fetchBookmarks() }
-            .navigationTitle("\(viewmodel.user?.name ?? "Profile")")
         }
     }
 }
